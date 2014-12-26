04905a3331b98ce04866eac19a9b0813a4544164
Gen Takahashi <g.takahashi@hachimenroppi.com>
2014-12-26 14:06:27 +0900

    初期設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+(1..100).each do |index|
+  FactoryGirl.create(:user, name: "name-#{index}")
+end
